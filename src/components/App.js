7d83d136b5a241a4e82d4b87fef4e0e4b293b80b
千田聖也 <wa718077@gmail.com>
2019-06-20 17:19:00 +0530

    finished project

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,26 @@
 import React from "react";
 import "../styles/App.css";
+import Header from "./Header";
+import { Switch, Route, Redirect } from "react-router-dom";
+import LinkList from "./LinkList";
+import CreateLink from "./CreateLink";
+import Login from "./Login";
+import Search from "./Search";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src.js</code> and to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="center w85">
+      <Header />
+      <div className="ph3 pv1 background-gray">
+        <Switch>
+          <Route exact path="/" render={() => <Redirect to="/new/1" />} />
+          <Route exact path="/create" component={CreateLink} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/search" component={Search} />
+          <Route exact path="/top" component={LinkList} />
+          <Route exact path="/new/:page" component={LinkList} />
+        </Switch>
+      </div>
     </div>
   );
 }
